1a084693dc24b1a914831d458e5cd677cc5b84f5
Callum Cameron <cjcameron7@gmail.com>
2017-07-18 08:32:04 +0200

    Use absolute path as EDITOR, so it works with sudoedit.

--- emacs-launchers.plugin.env.sh
+++ emacs-launchers.plugin.env.sh
@@ -1,5 +1,6 @@
 export PATH="${ANTIGEN_THIS_PLUGIN_DIR}:${PATH}"
 
 if type emacs >/dev/null 2>/dev/null; then
-    export EDITOR='emacs-daemon-blocking'
+    # We use the absolute path so sudoedit can use it, too
+    export EDITOR="$(which emacs-daemon-blocking)"
 fi
